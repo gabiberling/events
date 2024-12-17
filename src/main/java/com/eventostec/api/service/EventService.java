f8787ab0955d341c8b2f685ffbe1cb9ea7a0f5df
gabiberling <gabiberling@gmail.com>
2024-12-17 06:23:08 -0300

    feat: add aws

--- src/main/java/com/eventostec/api/service/EventService.java
+++ src/main/java/com/eventostec/api/service/EventService.java
@@ -3,6 +3,7 @@ package com.eventostec.api.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,9 @@ public class EventService {
     @Autowired
     private AmazonS3 s3Client;
 
+    @Autowired
+    private EventRepository repository;
+
     public Event createEvent(EventRequestDTO data) {
         String imgUrl = null;
 
@@ -37,6 +41,9 @@ public class EventService {
         newEvent.setEventUrl(data.eventUrl());
         newEvent.setDate(new Date(data.date()));
         newEvent.setImgUrl(imgUrl);
+        newEvent.setRemote(data.remote());
+
+        repository.save(newEvent);
 
         return newEvent;
     }
@@ -50,7 +57,7 @@ public class EventService {
             return s3Client.getUrl(bucketName, fileName).toString();
         } catch (Exception e){
             System.out.println("erro ao subir arquivo");
-            return null;
+            return "";
         }
     }
 
